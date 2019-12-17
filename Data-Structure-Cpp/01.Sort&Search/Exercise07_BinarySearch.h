93a5f6228d403d0b18583d7852d0a4134d40c8a5
Wang Cheng <bxwangcheng@gmail.com>
2019-12-17 20:45:18 +0800

    complete binary search

--- Data-Structure-Cpp/01.Sort&Search/Exercise07_BinarySearch.h
+++ Data-Structure-Cpp/01.Sort&Search/Exercise07_BinarySearch.h
@@ -1,5 +1,22 @@
 #include "Exercise00_SortTools.h"
 
-bool binary_search(vector<int>& x, int value) {
-	return true;
+int binary_search(vector<int>& x, int value) {
+	if (x.size() < 1 || &x == nullptr) {
+		return -1;
+	}
+	int l = 0;
+	int r = x.size()-1;
+	while (l < r) {
+		int mid = l + ((r - l) >> 2);
+		if (x[mid] < value) {
+			l = mid + 1;
+		}
+		else if (x[mid] > value) {
+			r = mid - 1;
+		}
+		else {
+			return mid;
+		}
+	}
+	return l;
 }
